e72a229755dc2586d474116844dfb054fc64b8af
KilianJ <69219356+gowstpm@users.noreply.github.com>
2025-01-24 14:59:05 +0100

    Ajout de la date d'arrivé et la date de départ pour l'employeur

---
+++ Personnel/src/test/java/testsUnitaires/testLigue.java
@@ -0,0 +1,26 @@
+package testsUnitaires;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import personnel.*;
+
+class testLigue 
+{
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	
+	@Test
+	void createLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertEquals("Fléchettes", ligue.getNom());
+	}
+
+	@Test
+	void addEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", null, null); 
+		assertEquals(employe, ligue.getEmployes().first());
+	}
+}

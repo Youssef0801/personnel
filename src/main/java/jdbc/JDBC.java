fbe1a10bc22616c36c108c66979692beeccc1d7c
KilianJ <69219356+gowstpm@users.noreply.github.com>
2025-03-14 15:56:49 +0100

    Added the insert for Employe JDBC.java

--- src/main/java/jdbc/JDBC.java
+++ src/main/java/jdbc/JDBC.java
@@ -87,4 +87,24 @@ public class JDBC implements Passerelle
 			throw new SauvegardeImpossible(exception);
 		}		
 	}
+
+	@Override
+	public int insert(Employe employe) throws SauvegardeImpossible 
+	{
+		try 
+		{
+			PreparedStatement instruction;
+			instruction = connection.prepareStatement("insert into employe (nom) values(?)", Statement.RETURN_GENERATED_KEYS);
+			instruction.setString(1, employe.getNom());		
+			instruction.executeUpdate();
+			ResultSet id = instruction.getGeneratedKeys();
+			id.next();
+			return id.getInt(1);
+		} 
+		catch (SQLException exception) 
+		{
+			exception.printStackTrace();
+			throw new SauvegardeImpossible(exception);
+		}		
+	}
 }

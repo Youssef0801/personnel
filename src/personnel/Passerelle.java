07d09cd0a4523110fbf1c70dafc974366872f82e
KilianJ <69219356+gowstpm@users.noreply.github.com>
2025-03-27 10:30:35 +0100

    Ajoute d'une méthode dans l'interface Passerelle pour permettre la suppression d'une ligue.

--- src/personnel/Passerelle.java
+++ src/personnel/Passerelle.java
@@ -8,5 +8,6 @@ public interface Passerelle
     public int insert(Employe employe) throws SauvegardeImpossible;
     public void update(Ligue ligue) throws SauvegardeImpossible;
     public void update(Employe employe) throws SauvegardeImpossible;
-    public void delete(Employe employe) throws SauvegardeImpossible; // Nouvelle méthode
+    public void delete(Employe employe) throws SauvegardeImpossible;
+    public void delete(Ligue ligue) throws SauvegardeImpossible; // Nouvelle méthode
 }
